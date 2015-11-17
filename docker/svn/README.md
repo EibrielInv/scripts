f21be2dee3284543e553933b273210677bb6165b
Eibriel <eibriel@yahoo.com.ar>
2015-11-17 09:26:09 -0300

    Simplifying svn docker

--- docker/svn/README.md
+++ docker/svn/README.md
@@ -1,7 +1,13 @@
+docker build -t svn:latest .
+docker create -l svn_kiribati --name=svn_kiribati -p=3690:3690 \
+        -v /svn/kiribati:/svn/kiribati svn:latest
+
+start svn_kiribati
+
+
+
+
+
 docker create -v /svn --name svndata ubuntu:14.04 /bin/true
 
 docker run -d --volumes-from svndata --name=svn_kiribati svn:latest
-
-docker build -t svn:latest .
-docker create -l svn_kiribati --name=svn_kiribati -p=3690:3690 svn:latest
-start svn_kiribati
--- docker/svn/init.sh
+++ docker/svn/init.sh
@@ -2,28 +2,6 @@
 
 umask 002
 
-if [ ! -d "/svn/kiribati" ];
-then
-
-    #mv /svn_tmp/kiribati /svn/kiribati
-
-    #RUN mkdir /svn/kiribati
-
-    chown svn:svn /svn
-    chmod g+w /svn
-    chmod g+s /svn
-
-    svnadmin create /svn/kiribati
-
-    #password-db = passwd
-
-    echo "[general]" > /svn/kiribati/conf/svnserve.conf
-    echo "password-db = passwd" >> /svn/kiribati/conf/svnserve.conf
-
-    echo "myname = mypasswd" >> /svn/kiribati/conf/passwd
-fi
-
 # RUN SERVER
 
-#python /bam/webservice/bam/manage.py runserver -h 0.0.0.0
 svnserve --daemon --foreground --root /svn/kiribati
